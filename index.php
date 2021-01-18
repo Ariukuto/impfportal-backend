bc0ad25f302c55483002dcc4ad13bf1409978d19
Ariukuto <sven.buettner93@gmx.de>
2021-01-18 21:36:52 +0100

    Schnell ohne Models und overkill JSON API erstellt

--- index.php
+++ index.php
@@ -1,25 +1,26 @@
 <?php
 
-require "./functions/createPasswordHash.php";
-require "./functions/getDataFromUrl.php";
-require "./createApiKey.php";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require "./database.php";
 require "./models/Impfwilliger.php";
 
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
-$data = getDataFromUrl($_REQUEST);
-$passwordhash = createPasswordHash($data["password"]);
-$apikey = createApiKey();
-$listennummer = getListenNummer($data);
-$dbConnection = new Database();
-$impfwilliger = new Impfwilliger($dbConnection);
-$impfwilliger->create(
-    $data["email"],
-    $data["ausweisnummer"],
-    $data["vorname"],
-    $data["nachname"],
-    $data["bday"],
-    $passwordhash,
-    $apikey,
-    $listennummer
-);
+$database = new PDO('mysql:host=localhost;dbname=impfportal', 'root', 'root');
+$sql = "SELECT * FROM impfwillige";
+$data = [];
+foreach ($database->query($sql) as $row) {
+    $data[] = $row;
+}
+if(count($data) > 0) {
+    echo json_encode($data);
+} else {
+    echo "Es konnten keine impfwilligen gefunden werden";
+}
+
+
